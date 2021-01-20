892ab16e940be185ed57bd41594ff3c8f144e2de
gdilthey <george.dilthey@gmail.com>
2021-01-20 04:08:33 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,14 @@
 class Person
-  def initialize(name, job)
-    @name = name
-    @job = job
+  def name=(new_name)
+    @name = new_name
   end
 
   def name
     @name
+  end
+
+  def job=(new_job)
+    @job = new_job
   end
 
   def job
